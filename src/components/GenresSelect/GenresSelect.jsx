f8c8e59a1b2dbd52d8929d1e0a003f0e911134a4
Nikola Jovanovic <nikolajovanovic.cr99@gmail.com>
2023-08-10 18:22:54 +0200

    update dropdown styles

--- src/components/GenresSelect/GenresSelect.jsx
+++ src/components/GenresSelect/GenresSelect.jsx
@@ -3,35 +3,54 @@ import { genres } from "../../helpers/genres";
 import "./genresselect.scss";
 
 const GenresSelect = ({ setSelectedGenre }) => {
+  const customStyles = {
+    placeholder: (baseStyles) => ({
+      ...baseStyles,
+      fontFamily: "Roboto, sans-serif",
+      textAlign: "left",
+      fontWeight: "400",
+      fontSize: "1rem",
+    }),
+    valueContainer: (baseStyles) => ({
+      ...baseStyles,
+      padding: "1rem",
+    }),
+    dropdownIndicator: () => ({
+      color: "#e50914",
+      margin: "0 1rem",
+      transform: "scale(1.5)",
+    }),
+    control: (baseStyles, state) => ({
+      ...baseStyles,
+      borderColor: "#464646",
+      borderWidth: "2px",
+      backgroundColor: "#00000056",
+      boxShadow: state.isActive ? "none" : "none",
+      cursor: "pointer",
+    }),
+    indicatorSeparator: (baseStyles) => ({
+      ...baseStyles,
+      display: "none",
+    }),
+    menuList: (baseStyles) => ({
+      ...baseStyles,
+      backgroundColor: "#000",
+      opacity: "90%",
+      color: "red",
+      fontFamily: "Roboto",
+      borderRadius: "5px",
+    }),
+    menuPortal: (baseStyles) => ({
+      ...baseStyles,
+      color: "aqua",
+      backgroundColor: "green",
+    }),
+  };
+
   return (
     <Select
-      styles={{
-        placeholder: (baseStyles) => ({
-          ...baseStyles,
-          fontFamily: "Roboto",
-          marginRight: "2rem",
-          padding: "1rem 0",
-        }),
-        dropdownIndicator: () => ({
-          color: "#e50914",
-          margin: "0 0.3rem",
-        }),
-        control: (baseStyles, state) => ({
-          ...baseStyles,
-          borderColor: "#464646",
-          borderWidth: "2px",
-          backgroundColor: "#00000056",
-          outline: "none",
-        }),
-        indicatorSeparator: (baseStyles) => ({
-          ...baseStyles,
-          display: "none",
-        }),
-        menuList: (baseStyles) => ({
-          ...baseStyles,
-          backgroundColor: "#00000056",
-        }),
-      }}
+      styles={customStyles}
+      defaultValue="All"
       placeholder="Select genre..."
       onChange={(value) => {
         let genreId = parseInt(value.value);
